7233473be1deb809cf6fc5cc71d5b79b4e8d8158
Steve Mansour <sman@stevemansour.com>
2021-03-10 00:16:47 -0800

    WIP

---
+++ graymusicfactory.com/gray.js
@@ -0,0 +1,40 @@
+/*global
+    document,
+*/
+
+"use strict";
+
+var app = {
+    menus: ["Home","Music","Video","Merch","About"],
+    links: ["/index","/music","/video","/merch","/about"]
+};
+
+function buildMenuString() {
+    var s = '<div class="topnav">';
+    for (var i = 0; i < app.menus.length; i++) {
+        var m = app.menus[i];
+        var n = app.links[i] + ".html";
+        s += '<a id="topMenuAnchor' + m + '" href="' + n +'">' + m + '</a>';
+    }
+    return s + '</div>';
+}
+
+function showMenu() {
+    document.write( buildMenuString() );
+}
+
+function highlightMenuItem(s) {
+    var j = app.menus.indexOf(s);  // may not be found
+    var x;
+    for (var i = 0; i < app.menus.length; i++) {
+        x = document.getElementById('topMenuAnchor'+app.menus[i]);
+        if (x == null) {
+            continue; // this should never happen
+        }
+        x.classList.remove("active");
+    }
+    if (j >= 0) {
+        x = document.getElementById('topMenuAnchor'+app.menus[j]);
+        x.classList.add("active");
+    }
+}
